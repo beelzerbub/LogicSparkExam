831b9c92c9c90ceed0207b163f71b012bec25425
ratchakran.p <Ratchakran.nut@gmail.com>
2021-02-08 18:31:10 +0700

    - Methods : Write create table migrate schema

--- migrations/20210208181550_create-categories-table.js
+++ migrations/20210208181550_create-categories-table.js
@@ -1,3 +1,10 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("categories", (tbl) => {
+    tbl.increments("id").primary();
+    tbl.string("category_name", 50).notNullable();
+    tbl.timestamp("create_at").notNullable().defaultTo(knex.fn.now());
+    tbl.timestamp("update_at").notNullable().defaultTo(knex.fn.now());
+  });
+};
 
 exports.down = function (knex) {};
