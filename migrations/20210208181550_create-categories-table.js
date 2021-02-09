922e83595bc7fee987c87e8ef8950c89b60fb3b4
ratchakran.p <Ratchakran.nut@gmail.com>
2021-02-09 15:01:26 +0700

    - Files : Migrate database js working well

--- migrations/20210208181550_create-categories-table.js
+++ migrations/20210208181550_create-categories-table.js
@@ -1,10 +1,42 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("categories", (tbl) => {
-    tbl.increments("id").primary();
-    tbl.string("category_name", 50).notNullable();
-    tbl.timestamp("create_at").notNullable().defaultTo(knex.fn.now());
-    tbl.timestamp("update_at").notNullable().defaultTo(knex.fn.now());
-  });
+  return knex.schema
+    .createTable("categories", (table) => {
+      table.increments("id").primary();
+      table.string("category_name", 50).notNullable();
+      table.timestamp("create_at").notNullable().defaultTo(knex.fn.now());
+      table.timestamp("update_at").notNullable().defaultTo(knex.fn.now());
+    })
+    .createTable("products", (table) => {
+      table.increments("id").primary();
+      table.string("product_name", 50).notNullable();
+      table.integer("product_group_id").unsigned();
+      table.timestamp("create_at").notNullable().defaultTo(knex.fn.now());
+      table.timestamp("update_at").notNullable().defaultTo(knex.fn.now());
+    })
+    .createTable("product_groups", (table) => {
+      table.increments("id").primary();
+      table.integer("product_id").unsigned();
+      table.integer("categories_id").unsigned();
+      table
+        .foreign("product_id")
+        .references("id")
+        .inTable("products")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .foreign("categories_id")
+        .references("id")
+        .inTable("categories")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.timestamp("create_at").notNullable().defaultTo(knex.fn.now());
+      table.timestamp("update_at").notNullable().defaultTo(knex.fn.now());
+    });
 };
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex
+    .dropTableIfExits("product_groups")
+    .dropTableIfExits("products")
+    .dropTableIfExits("categories");
+};
