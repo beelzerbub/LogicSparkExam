a931b8084d9572b70bfa3d148d0b4278eec62f99
ratchakran.p <Ratchakran.nut@gmail.com>
2021-02-11 15:10:01 +0700

    - Methods : adding category now working well and support multiple values

--- src/controller/categoryController.ts
+++ src/controller/categoryController.ts
@@ -1,11 +1,45 @@
-import { from } from "rxjs";
+import { concat, from, of } from "rxjs";
+import { catchError, concatMap, map, tap, toArray } from "rxjs/operators";
 import { CategoryData, ICategory } from "../models/categoryData";
 import { add } from "../models/dbHelper";
 
 const tableName = "categories";
 
-export const addCategory = (input: ICategory) => {
-  const data = new CategoryData(input);
-  const result = add(tableName, data);
+export const addCategory = (input: ICategory | ICategory[]) => {
+  let result: Promise<
+    Array<
+      | {
+          id: number;
+          category_name: string;
+        }
+      | {
+          error: string;
+        }
+    >
+  >;
+  result = from(Array.isArray(input) ? input : [input])
+    .pipe(
+      concatMap((each: ICategory) => {
+        const data = new CategoryData(each);
+        return from(add(tableName, data))
+          .pipe(
+            map((queryResult) => ({
+              id: queryResult,
+              category_name: data.category_name,
+            })),
+            catchError((err) => {
+              return of({ error: `${err.message}` });
+            })
+          )
+          .toPromise();
+      }),
+      toArray(),
+      tap((res) => {
+        if (process.env.DEV) {
+          console.log("insert result : ", res);
+        }
+      })
+    )
+    .toPromise();
   return result;
 };
